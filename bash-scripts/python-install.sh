6a382bb004a09280cc2b1d6d1854da3a5aecc7a7
dmytro <skiff120@gmail.com>
2019-12-09 01:50:52 +0200

    commit msh

--- bash-scripts/python-install.sh
+++ bash-scripts/python-install.sh
@@ -46,7 +46,7 @@ pip install kafka-python
 pip3 install kafka-python
 cd ~/Downloads
 # Uncomment for first launch
-#wget http://www-us.apache.org/dist/kafka/2.2.1/kafka_2.12-2.2.1.tgz
-#tar xzf kafka_2.12-2.2.1.tgz
-#sudo mv kafka_2.12-2.2.1 /usr/local/kafka
+wget http://www-us.apache.org/dist/kafka/2.2.1/kafka_2.12-2.2.1.tgz
+tar xzf kafka_2.12-2.2.1.tgz
+sudo mv kafka_2.12-2.2.1 /usr/local/kafka
 bash zookeeper-server-start.sh
