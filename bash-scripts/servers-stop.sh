48245e571015d3f176e3ba60ca0b2f3f75814ec1
dmytro <skiff120@gmail.com>
2019-12-08 23:16:40 +0200

    commit msh

--- bash-scripts/servers-stop.sh
+++ bash-scripts/servers-stop.sh
@@ -1,2 +1,4 @@
-/usr/local/kafka//bin/kafka-server-stop.sh
-/usr/local/kafka//bin/kafka-server-stop.sh
+/usr/local/kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic topic5
+/usr/local/kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic topic5
+/usr/local/kafka/bin/kafka-server-stop.sh
+/usr/local/kafka/bin/kafka-server-stop.sh
--- tests/kafkaTest.py
+++ tests/kafkaTest.py
@@ -1,4 +1,4 @@
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:1234')
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
 for _ in range(100):
 	producer.send('topic5', b'some_message_bytes').get(timeout=1000)
