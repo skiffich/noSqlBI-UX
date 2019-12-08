a76d3071b82860bcc0366c8d4704d18246b34198
dmytro <skiff120@gmail.com>
2019-12-08 21:14:12 +0200

    commit msh

--- bash-scripts/launch.sh
+++ bash-scripts/launch.sh
@@ -1,8 +1,5 @@
 echo off
 
-export PATH="$PATH:#PWD"
-chmod +x "$PWD/python-install.sh"
-
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt update
@@ -11,8 +8,10 @@ sudo apt update
 echo installing vim
 sudo apt install vim -y
 
+sudo apt-get install unzip -y
+
 echo installing git
-sudo apt install git -y
+sudo apt-get install git -y
 git --version
 git config --global user.name "dm"
 git config --global user.email "skiff120@gmail.com"
@@ -21,5 +20,10 @@ echo initialize git
 git init
 git remote add origin https://github.com/skiffich/noSqlBI-UX.git
 git pull origin master
+
+cd bash-scripts
+
+export PATH="$PATH:#PWD"
+chmod +x "$PWD/python-install.sh"
 
 python-install.sh
