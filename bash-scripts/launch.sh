3a4533ee319213c892a00f6b3f2921a8456136de
dmytro <skiff120@gmail.com>
2019-12-09 09:38:01 +0200

    commit msh

--- bash-scripts/launch.sh
+++ bash-scripts/launch.sh
@@ -10,11 +10,11 @@ sudo apt-get install curl -y
 curl --version
 sudo apt-get install unzip -y
 
-'''
+
 sudo apt install openjdk-8-jdk -y
 sudo apt install openjdk-8-jre -y
 java -version
-#curl https://1drv.ms/u/s!ApAsseJ88mCxk9ARyQ1nThCisoq0fQ?e=KqPenJ
+wget https://8pjqmw.db.files.1drv.com/y4mmlUIX9Haiuc74SfFe8_qafkzCjD756K77hEaRpFljKs9hmi3OPrwD0WGnH3cTPvRGac8rp2ON6-MvNnRCTtD9iuOu4anj5EgXNjGRt-cbS9Tzj-6EV1b807eB5LvFzTQIFn1m0yhTRVbu45IX3msRmerAcagb3XZtQ13mN_hxc2JHHidcEB2Di5vUrY_XnAe/jdk-8u231-linux-x64.tar.gz?download&psid=1
 sudo mkdir /usr/lib/jvm/
 sudo tar -zxvf jdk-8u231-linux-x64.tar.gz -C /usr/lib/jvm/
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.8.0_231/bin/java 3
@@ -25,6 +25,29 @@ export JRE_HOME=/usr/lib/jvm/jdk1.8.0_231/jre/
 export J2SDKDIR=/usr/lib/jvm/jdk1.8.0_231/
 export J2REDIR=/usr/lib/jvm/jdk1.8.0_231/jre/
 java --version
+
+'''
+#ElasticSearch
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch.service
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get update && sudo apt-get install kibana
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable kibana.service
+sudo systemctl start kibana.service
+sudo systemctl status kibana.service
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.5.0-linux-x86_64.tar.gz
+tar xzvf metricbeat-7.5.0-linux-x86_64.tar.gz
+#kafka912
 '''
 
 echo installing git
